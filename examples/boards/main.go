8d0e6380cbe8a704062cde47d91334d05a5c4135
Leonardo Comelli <leonardo.comelli@gmail.com>
2019-05-25 23:37:57 -0300

    refactoring examples

--- examples/boards/main.go
+++ examples/boards/main.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/leocomelli/jira"
 )
 
-func main() {
+var url, user, pass string
 
-	url := os.Getenv("JIRA_URL")
-	user := os.Getenv("JIRA_USER")
-	pass := os.Getenv("JIRA_PASS")
+func init() {
+	url = os.Getenv("JIRA_URL")
+	user = os.Getenv("JIRA_USER")
+	pass = os.Getenv("JIRA_PASS")
+}
+
+func main() {
 
 	tp := &jira.BasicAuthTransport{
 		Username: user,
@@ -23,6 +28,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	listBoards(client)
+	getBoard(client)
+	listEpics(client)
+	listBacklogIssues(client)
+	listSprints(client)
+}
+
+func listBoards(client *jira.Client) {
+	fmt.Println("LIST BOARD...")
 
 	opts := &jira.ListBoardsOptions{
 		ProjectKeyOrID: "CBD",
@@ -34,34 +49,62 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("max results: %d\n", resp.MaxResults)
-	fmt.Printf("start at: %d\n", resp.StartAt)
-	fmt.Printf("is last: %v\n", resp.IsLast)
-	fmt.Println("boards: ")
+	fmt.Printf("\tmax results: %d\n", resp.MaxResults)
+	fmt.Printf("\tstart at: %d\n", resp.StartAt)
+	fmt.Printf("\tis last: %v\n", resp.IsLast)
 
 	for _, b := range boards {
-		fmt.Printf("\t %d - %v\n", b.ID, b.Name)
+		fmt.Printf("\t%d - %s\n", b.ID, b.Name)
+	}
+}
 
-		sprints, _, err := client.Boards.ListSprints(context.Background(), b.ID, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("sprints: ")
-		for _, s := range sprints {
-			fmt.Printf("\t id: %d, name: %s, state: %s, start: %v, end: %v, complete: %v\n", s.ID, s.Name, s.State, s.Start, s.End, s.Complete)
-		}
-
-		epics, _, err := client.Boards.ListEpics(context.Background(), b.ID, &jira.ListEpicsOptions{})
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("epics: ")
-		for _, e := range epics {
-			fmt.Printf("\t id: %d, name: %s, done: %v\n", e.ID, e.Name, e.Done)
-		}
+func getBoard(client *jira.Client) {
+	fmt.Println("GET BOARD...")
+	board, _, err := client.Boards.GetBoard(context.Background(), 2881)
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	fmt.Printf("\t%d - %s - %s\n", board.ID, board.Name, board.Type)
+}
+
+func listEpics(client *jira.Client) {
+	fmt.Println("LIST EPICS...")
+
+	epics, _, err := client.Boards.ListEpics(context.Background(), 2881, &jira.ListEpicsOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range epics {
+		fmt.Printf("\tid: %d, name: %s, done: %v\n", e.ID, e.Name, e.Done)
+	}
+}
+
+func listBacklogIssues(client *jira.Client) {
+	fmt.Println("BACKLOG...")
+
+	issues, _, err := client.Boards.ListBacklogIssues(context.Background(), 2881, &jira.ListBacklogIssuesOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, i := range issues {
+		fmt.Printf("\tid: %s, key: %s, reporter: %s, status: %s\n",
+			i.ID, i.Key, i.Fields.Reporter.DisplayName, i.Fields.Status.Name)
+	}
+}
+
+func listSprints(client *jira.Client) {
+	fmt.Println("LIST SPRINTS...")
+
+	sprints, _, err := client.Boards.ListSprints(context.Background(), 2881, &jira.ListSprintsOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, s := range sprints {
+		fmt.Printf("\tid: %d, name: %s, state: %s, start: %v, end: %v\n",
+			s.ID, s.Name, s.State, s.Start.Format(time.RFC3339Nano), s.End.Format(time.RFC3339Nano))
+	}
 }
